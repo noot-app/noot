ae2279e1d9a783ece7b84154b89517f188e5d26c
GrantBirki <grant.birkinbine@gmail.com>
2025-09-12 12:21:37 -0700

    this works

--- apps/web/capacitor.config.ts
+++ apps/web/capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.nootapp',
   appName: 'Noot',
-  webDir: 'dist'
+  webDir: 'www',
+  server: {
+    url: 'http://192.168.1.180:3000', // Your Mac's IP address
+    cleartext: true, // Allow HTTP for local dev
+    allowNavigation: ['*']
+  }
 };
 
 export default config;
